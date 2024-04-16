170da47ec8909e4b6b8fa5d3c6b93b5b828a86bd
TajahH <167078781+TajahH@users.noreply.github.com>
2024-04-16 12:06:14 +0100

    Create multiply.js

---
+++ multiply.js
@@ -0,0 +1,8 @@
+// Task 2
+// Use a loop to print out the squares for the numbers between 1 and 100.
+
+const readline = require("readline-sync");
+
+for (let i = 1; i <= 100; i++){
+    console.log(Math.pow(i,2))
+}
